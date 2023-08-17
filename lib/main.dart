516ad4e79b7d4740b2199c8ef7bc25386180ef33
Dhruv <dhruvsoni285@gmail.com>
2023-08-17 11:49:59 +0530

    initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_provider/HomeScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Provider on MVVM Architecture Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: ThemeMode.system,
+      home: HomeScreen(),
+    );
+  }
+}
